e61da2f19faf70775edeb9bb9af83ccb7abb7908
Priyav <priyavpithia7@gmail.com>
2024-10-16 21:01:30 +0300

    Fix Auth page and update import path

---
+++ pages/Auth.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AuthForm } from '../components/AuthForm';
+
+export default function Auth() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-background">
+      <AuthForm />
+    </div>
+  );
+}
